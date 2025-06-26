55acd53a4c072b6af5b25f8d30baa582aa5c13cd
NurulNajatMony <nurulmony505@gmail.com>
2025-06-26 14:49:24 +0700

    alokasi dan inisialisasi node baru

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -31,4 +31,13 @@ public:
         int x;
         cout << "Masukkan nilai: ";
         cin >> x;
-   
+
+         // Step 1: Allocate memory for the new node
+        Node *newNode = new Node();
+
+        // Step 2: Assign value to the data field of new node 
+        newNode->info = x;
+
+        // Step 3: Make the left and right child of the new node point
+        newNode->leftchild = nullptr;
+        newNode->rightchild = nullptr;
