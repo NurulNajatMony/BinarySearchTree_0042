c83effa8ebd0cd8b3e7e810867fd2b2ff829365f
NurulNajatMony <nurulmony505@gmail.com>
2025-06-26 14:54:02 +0700

    Menyisipkan elemen baru berdasarkan nilai lebih kecil atau lebih besar

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -46,3 +46,36 @@ public:
         Node *parent =  nullptr;
         Node *currentNode = nullptr;
         search(x, parent, currentNode);
+
+        // Step 5: If parent is NULL (Tree is empty)
+        if (parent == nullptr)
+        {
+            // 5a: Mark the new node as ROOT
+            ROOT = newNode;
+
+            // 5b: Exit
+            return;
+        }
+
+        // Step 6: If the value in the data field of new node is less than that of parent
+        if (x < parent->info)
+        {
+            // 6a: Make the left child of parent point to the new node
+            parent->leftchild = newNode;
+
+            // 6b: Exit
+            return;
+        }
+        // Step 7: If the value in the data field of the
+        //new node is greater than that of the parent
+        else if (x > parent->info)
+        {
+            // 7a: Make the right child of parent point to the new node 
+            parent->rightchild = newNode;
+            // 7b: Exit
+            return;
+        }
+
+
+    }
+
